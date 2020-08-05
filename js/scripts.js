d8a8561d9cdb863beb59dc158df732d35a8a7cf8
virsail <virsaileric@gmail.com>
2020-08-05 12:05:54 +0300

    add a function for the toggle

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(".toggle").click(function() {
+      $("#shown-design").toggle();
+      $("#hidden-design").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".toggle1").click(function() {
+      $("#shown-development").toggle();
+      $("#hidden-development").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".toggle2").click(function() {
+      $("#shown-product").toggle();
+      $("#hidden-product").toggle();
+    });
+  });
