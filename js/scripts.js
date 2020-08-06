0b5fc4f0259702923de1f3c3720f3ab80aee56f3
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-08-06 13:20:21 +0300

    add images to be used in creating the gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-    $(".event1").mouseover(function() {
-      $("#j1").show()
-    })
-    $(".event1").mouseleave(function(){
-      $("#j1").hide()
-    })
-
 function submitForm(event) {
     var form = new FormData(event.target)
     var name = $("input#name").val();
